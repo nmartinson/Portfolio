971d20ebb6d01c18742292070f1d775c07dbe9c4
Nick Martinson <nickmartinson986@gmail.com>
2016-11-13 12:52:10 -0800

    Added about page

---
+++ app/components/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import axios from 'axios';
+import { Link } from 'react-router';
+
+
+const About = () => {
+  var photos = [];
+
+
+  return (
+    <div>
+    
+      <h1>About Me</h1>
+      <p>Nice to meet you! Born and raised in Bettendorf, Iowa,
+      engineers by trade, adventurers at heart.  
+      We live in Chicago, IL, but that’s not where it all began. At the ripe young age of 4 Nick was kicking up dirt racing 
+      motocross and Angelina was living wild and free in a Russian orphanage.  By college Nick was backpacking continents, and working
+       on his photography</p>
+    
+    </div>
+  )
+}
+
+export default About;
