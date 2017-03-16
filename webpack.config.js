b96f26d89b5d1115a9a9914e435b6c726f076d5e
Nick Martinson <nickmartinson986@gmail.com>
2017-03-15 21:34:01 -0700

    changed public

--- webpack.config.js
+++ webpack.config.js
@@ -60,7 +60,7 @@ module.exports = [
         output: {
             path: path.join(__dirname, 'public'),
             filename: 'bundle.js',
-            publicPath: 'wwww.portfolio.boundless-journey.com/'
+            publicPath: '/public/'
         },
         module: {
             loaders: [
